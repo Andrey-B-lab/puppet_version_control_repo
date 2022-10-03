709872a473edccaaac0b7e23512d34d989e260fa
Andrey Byhalenko <72738126+Andrey-B-lab@users.noreply.github.com>
2022-10-03 14:23:19 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,5 +1,6 @@
 node default {
   file {'/root/README':
     ensure => file,
+    content => 'I'm using r10k on my demo RedHat vm with Puppet to manage my production environment configuration in this repo.',
   }
 }
