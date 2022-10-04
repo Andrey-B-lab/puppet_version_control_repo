75a6170439a53029ab4ac9bc29406607d6a69542
Andrey Byhalenko <72738126+Andrey-B-lab@users.noreply.github.com>
2022-10-04 11:37:02 +0300

    Update ssh_server.pp

--- site/profile/manifest/ssh_server.pp
+++ site/profile/manifest/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDEzzdIAmE1gQIhIrm828pTwouIM6LJpru4j2vcpiWVr9Ua9/8OH1xxvf/xLEShSHUXufSaOXSrd9llOSanndbrCDh4pKQXhG7Ot5Waf3A/FlcR0gjGA1QMkOu7YonOQU1IuymzI0N05+8DXpIa7sv8ch3uYObrjilHRu4kQdp185W7euoPg2DVB3++UCmacJ0yjxNxIyLBH2HqayjcYORxlcCc4Z0zq2CmgofJgSsU9QvMHNGJ4frCP0kiBWEu28QwbMYqupUY0cAskQCE7bvCJATOfKXkdfK+9uZyqCNX9mzBA9gNS1hVHgl04xiFEAECi+zn/d44ARbz5QkgVB0d',
+    key    => '${SSH_KEY_PUPPET}',
   }  
 }
