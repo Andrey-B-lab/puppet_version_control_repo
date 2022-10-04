a03dcda7f8174846ba4ce4f70f2327ca3699ebd0
Andrey Byhalenko <72738126+Andrey-B-lab@users.noreply.github.com>
2022-10-04 11:07:02 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -5,7 +5,7 @@ node 'master.puppet.VirtuialMachine' {
   include role::master_server
   file { '/root/README':
   ensure  => file,
-  content => $fqdn,
+  content => "Welcome to {$fqdn}"\n,
   }
 }
 
