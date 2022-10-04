401488e3aabad20c9a5d399ef67d74ae849c6eb0
Andrey Byhalenko <72738126+Andrey-B-lab@users.noreply.github.com>
2022-10-04 11:05:41 +0300

    Update site.pp

--- manifest/site.pp
+++ manifest/site.pp
@@ -1,12 +1,12 @@
 node default {
-  file { '/root/README':
-    ensure  => file,
-    content => 'I'm using r10k on my demo RedHat vm with Puppet to manage my production environment configuration in this repo.',
-    owner   => 'root',
-   }
 }
+
 node 'master.puppet.VirtuialMachine' {
   include role::master_server
+  file { '/root/README':
+  ensure  => file,
+  content => $fqdn,
+  }
 }
 
 node /^web/ {
