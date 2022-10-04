ac7cb8f33117d68ecd22994d6e58eb36639c58b4
Andrey Byhalenko <72738126+Andrey-B-lab@users.noreply.github.com>
2022-10-04 11:33:40 +0300

    Update agent_nodes.pp

--- site/profile/manifest/agent_nodes.pp
+++ site/profile/manifest/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node{'web.puppet.VirtualMachine':}
   dockeragent::node{'db.puppet.VirtualMachine':}
+  host {'web.puppet.VirtualMachine':
+    ensure => present,
+    ip     => '172.18.0.2',   ## In the real PRD environment I will use DNS.
+  }
+  host {'db.puppet.VirtualMachine':
+    ensure => present,
+    ip     => '172.18.0.3',   ## In the real PRD environment I will use DNS.
+  }
 }
